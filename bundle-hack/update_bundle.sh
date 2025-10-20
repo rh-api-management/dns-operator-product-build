1ca743907bd20e8496e51801e9c42b331e845606
Joseph Kiely <jkiely@redhat.com>
2025-10-20 13:11:56 +0100

    Fix issues with dns-operator bundle for RHCL 1.2.0

--- bundle-hack/update_bundle.sh
+++ bundle-hack/update_bundle.sh
@@ -14,11 +14,11 @@ if [[ "${development:-}" == "true" ]]; then
     echo "Development bundle: leaving quay.io pullspecs unchanged"
 elif [[ "${stage:-}" == "true" ]]; then
     # Use stage pullspecs
-    sed -i -e "s|quay.io/redhat-user-workloads/api-management-tenant/rhcl-1-1-dns-operator|${DNS_OPERATOR_PULLSPEC_STAGE}|g" \
+    sed -i -e "s|quay.io/redhat-user-workloads/api-management-tenant/rhcl-1-2-dns-operator|${DNS_OPERATOR_PULLSPEC_STAGE}|g" \
         "${CSV_FILE}"
 else
     # Use production pullspecs
-    sed -i -e "s|quay.io/redhat-user-workloads/api-management-tenant/rhcl-1-1-dns-operator|${DNS_OPERATOR_PULLSPEC}|g" \
+    sed -i -e "s|quay.io/redhat-user-workloads/api-management-tenant/rhcl-1-2-dns-operator|${DNS_OPERATOR_PULLSPEC}|g" \
         "${CSV_FILE}"
 fi
 
